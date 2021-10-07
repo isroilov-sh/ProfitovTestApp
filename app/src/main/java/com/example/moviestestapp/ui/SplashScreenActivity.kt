a236b720858731c8a59de1dd45aab493483e0f90
isroilov <isroilov.sh.47@gmail.com>
2021-10-08 02:30:41 +0300

    UI-changes

--- app/src/main/java/com/example/moviestestapp/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/example/moviestestapp/ui/SplashScreenActivity.kt
@@ -8,7 +8,6 @@ import com.example.moviestestapp.R
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
         startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
